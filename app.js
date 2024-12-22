9037e180ccd5c90715bc30d271ef5d71c8198959
swinswinning <asw1nz@hotmail.com>
2024-12-22 13:13:35 +0100

    changes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { fetchAndSaveData } = require("./backend/helpers")
-const { fetchAPIData } = require("./backend/services/cmc")
-const { createTable, getAllData, clearDatabase, dropTable, getUcids, insertData } = require("./backend/services/dbservices")
+const { createTable, getAllData, clearDatabase, dropTable, getUcids } = require("./backend/services/dbservices")
 const cron = require('node-cron');
 const { create } = require('domain');
 
@@ -15,12 +14,13 @@ app.set('views', path.join(__dirname, 'frontend/views'));
 
 app.use(express.static(path.join(__dirname, 'frontend/public')));
 
-// Schedule a cron job to run every 15 minutes
-cron.schedule('*/15 * * * *', async () => {
+// Schedule a cron job to run every 5 minutes
+cron.schedule('*/5 * * * *', async () => {
   try {
     console.log('Fetching and saving data...');
-    data = await fetchAndSaveData(); 
-    
+    numresults = await fetchAndSaveData(); 
+    await clearDatabase(numresults);
+
   } catch (error) {
     console.error('Error fetching or saving data:', error);
   }
@@ -45,16 +45,16 @@ app.get('/', async (req, res) => {
   }
 });
 
-  app.post('/clearDatabase', async (req, res) => {
-    try {
-      const message = await clearDatabase();
+// app.post('/clearDatabase', async (req, res) => {
+//     try {
+//       const message = await clearDatabase();
      
-      res.status(200).send({ message }); // Send a proper status code (200 OK)
-      } catch (error) {
-        console.error('Error deleting data:', error);
-        res.status(500).send({ error: error.message || 'An unknown error occurred' });
-      }
-});
+//       res.status(200).send({ message }); // Send a proper status code (200 OK)
+//       } catch (error) {
+//         console.error('Error deleting data:', error);
+//         res.status(500).send({ error: error.message || 'An unknown error occurred' });
+//       }
+// });
 
 app.post('/dropTable', async (req, res) => {
   try {
@@ -67,31 +67,31 @@ app.post('/dropTable', async (req, res) => {
     }
 });
 
-app.post('/get-from-db', async (req, res) => {
-  try {
-    records = await getAllData()
-    // console.log(records.length)
-    res.json(records)
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Server error');
-  }
-});
+// app.post('/get-from-db', async (req, res) => {
+//   try {
+//     records = await getAllData()
+//     // console.log(records.length)
+//     res.json(records)
+//   } catch (error) {
+//     console.error('Error:', error);
+//     res.status(500).send('Server error');
+//   }
+// });
 
   app.post('/fetch-save-get', async (req, res) => {
     try {
-      // Step 1: Fetch data from an API
-        data = await fetchAPIData();  
-        // console.log(data != null)      
-  
-      // Step 2: Save fetched data into the SQLite database
-        results = await insertData(data)
-        // console.log(results)
+     // Fetch data from a cmc api and save into db
+      num_results = await fetchAndSaveData() // return number of coins to track
+      
 
-      // step 3: fetch all records from the database to send back to the client.
-      records = await getAllData()
-      // console.log(records.length)
-      res.json(records)
+      // Clear old records from the database
+      await clearDatabase(num_results);
+
+      // fetch all records from the database 
+      records = await getAllData();    
+
+     res.json(records) // send all db records back to the client as json.
+
     } catch (error) {
       console.error('Error:', error);
       res.status(500).send('Server error');
@@ -110,5 +110,5 @@ app.get('/get-ucids', async (req, res) => {
   });
 
 app.listen(port, '0.0.0.0' , () => {
-    console.log('Server running at http://localhost:3000');
+    console.log(`Server running at http://localhost:${port}`);
 });
